5deb411c71a7209b5aef66d8c423b74200135fac
Shelby Cumings <s.rs.cumings@txstate.edu>
2023-05-20 00:33:30 -0500

    Getters and setters
    Added methods to set and get values of class variables

--- Sign.java
+++ Sign.java
@@ -1,7 +1,7 @@
 
 // Stores sign characteristics in class variables
-// domHandshape: handshape of dominant hand based on handshape codes
-// baseHandshape: handshape of non-dominant (base) hand based on handshape codes
+// domHandshape: handshape of dominant hand per handshape codes
+// baseHandshape: handshape of non-dominant (base) hand per handshape codes
 // twoHanded: "Y" if both hands are used, "N" if one-handed
 // symmetrical: "Y" if symmetrical, "N" if asymmetrical
 // alternating: "Y" if alternating movement, "N" otherwise
@@ -10,6 +10,7 @@
 // match: number of fields that match search query
 
 public class Sign {
+    private String name;                // Sign name in English
     private String domHandshape;        // variable string
     private String baseHandshape;       // variable string
     private String twoHanded;           // Y/N
@@ -44,6 +45,86 @@ public class Sign {
         proximity = prox;
         movement = mov;
         match = 0;
+    }
+
+    public void setName(String signName) {
+        name = signName;
+    }
+
+    public void setDom(String handshape) {
+        domHandshape = handshape;
+    }
+
+    public void setBase(String handshape) {
+        baseHandshape = handshape;
+    }
+
+    public void setTwoHanded(String handedness) {
+        twoHanded = handedness;
+    }
+
+    public void setSymmetry(String symmetry) {
+        symmetrical = symmetry;
+    }
+
+    public void setAlternating (String alt) {
+        alternating = alt;
+    }
+
+    public void setRepetition(String rep) {
+       repeated = rep;
+    }
+
+    public void setProximity(String prox) {
+        proximity = prox;
+    }
+
+    public void setMovement(String mov) {
+        movement = mov;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDomHandshape() {
+        return domHandshape;
+    }
+
+    public String getBaseHandshape() {
+        return baseHandshape;
+    }
+
+    public String getHandedness() {
+        return twoHanded;
+    }
+
+    public String getSymmetry() {
+        return symmetrical;
+    }
+
+    public String getAlternating() {
+        return alternating;
+    }
+
+    public String getRepetition() {
+        return repeated;
+    }
+
+    public String getProximity() {
+        return proximity;
+    }
+
+    public String getMovement() {
+        return movement;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public void incrementMatch() {
+        match++;
     }
     
 }
